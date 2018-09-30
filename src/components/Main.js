de249d2771df42b99be61e42f9313dbda2774acf
Zihan Yi <z4yi@ucsd.edu>
2018-09-30 02:46:08 -0700

    gg

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Main.css"
+
+export class Main extends React.Component {
+    render() {
+        return (
+            <div className='main'>
+                <div>Profile</div>
+                <div>ShotChart</div>
+            </div>
+        );
+    }
+}
